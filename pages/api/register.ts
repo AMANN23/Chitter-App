6506810d21394c430c9b481080ecafe6aed7c355
Adnan Mann <adnan_mann@outlook.com>
2023-10-04 17:21:22 +0100

    Checking to see if user is logged in

---
+++ pages/api/register.ts
@@ -0,0 +1,20 @@
+import bcrypt from "bcrypt";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "POST") {
+    return res.status(405).end();
+  }
+
+  try {
+    const { email, username, name, password } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).end();
+  }
+}
