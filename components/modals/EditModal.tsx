e32469ccea819e75ca9dc4c31d594122c50fcca2
Adnan Mann <adnan_mann@outlook.com>
2023-10-06 14:15:09 +0100

    Returning modal function

--- components/modals/EditModal.tsx
+++ components/modals/EditModal.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 import useCurrentUser from "@/hooks/useCurrentUser";
 import useUser from "@/hooks/useUser";
@@ -9,11 +10,34 @@ const EditModal = () => {
   const { mutate: mutateFetchUser } = useUser(currentUser?.id);
   const editModal = useEditModal();
 
-  const [profileImage, setProfileImage] = useState();
-  const [coverImage, setCoverImage] = useState();
-  const [name, setName] = useState();
-  const [username, setUsername] = useState();
-  const [bio, setBio] = useState();
+  const [profileImage, setProfileImage] = useState("");
+  const [coverImage, setCoverImage] = useState("");
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [bio, setBio] = useState("");
+
+  useEffect(() => {
+    setProfileImage(currentUser?.profileImage);
+    setCoverImage(currentUser?.coverImage);
+    setName(currentUser?.name);
+    setUsername(currentUser?.username);
+    setBio(currentUser?.bio);
+  }, [
+    currentUser?.name,
+    currentUser?.username,
+    currentUser?.bio,
+    currentUser?.profileImage,
+    currentUser?.coverImag,
+  ]);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onSubmit = useCallback(async () => {
+    try {
+    } catch (error) {
+      toast.error("something went wrong");
+    }
+  }, []);
 
   return (
     <>
