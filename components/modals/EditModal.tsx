f4b187abc8604de7073a33d2011a652647dcc6b6
Adnan Mann <adnan_mann@outlook.com>
2023-10-06 14:16:06 +0100

    Edit function working

--- components/modals/EditModal.tsx
+++ components/modals/EditModal.tsx
@@ -4,10 +4,12 @@ import toast from "react-hot-toast";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import useUser from "@/hooks/useUser";
 import useEditModal from "@/hooks/useEditModal";
+import axios from "axios";
+import Modal from "../Modal";
 
 const EditModal = () => {
   const { data: currentUser } = useCurrentUser();
-  const { mutate: mutateFetchUser } = useUser(currentUser?.id);
+  const { mutate: mutateFetchedUser } = useUser(currentUser?.id);
   const editModal = useEditModal();
 
   const [profileImage, setProfileImage] = useState("");
@@ -34,15 +36,44 @@ const EditModal = () => {
 
   const onSubmit = useCallback(async () => {
     try {
+      setIsLoading(true);
+
+      await axios.patch("/api/edit", {
+        name,
+        username,
+        bio,
+        profileImage,
+        coverImage,
+      });
+      mutateFetchedUser();
+
+      toast.success("Updated");
+
+      editModal.onClose();
     } catch (error) {
-      toast.error("something went wrong");
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
     }
-  }, []);
+  }, [
+    bio,
+    name,
+    username,
+    profileImage,
+    coverImage,
+    editModal,
+    mutateFetchedUser,
+  ]);
 
   return (
-    <>
-      <div></div>
-    </>
+    <Modal
+      disabled={isLoading}
+      isOpen={editModal.isOpen}
+      title="Edit your profile"
+      actionLabel="Save"
+      onClose={editModal.onClose}
+      onSubmit={onSubmit}
+    />
   );
 };
 
