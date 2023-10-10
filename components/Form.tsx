658ba1567633017d484dc239ce77da5ee4486070
Adnan Mann <adnan_mann@outlook.com>
2023-10-10 16:58:13 +0100

    Creating body field

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,4 +1,21 @@
-const Form = () => {
+import useCurrentUser from "@/hooks/useCurrentUser";
+import useLoginModal from "@/hooks/useLoginModal";
+import usePosts from "@/hooks/usePosts";
+import useRegisterModal from "@/hooks/useRegisterModal";
+
+interface FormProps {
+  placeholder: string;
+  isComment?: boolean;
+  postId?: string;
+}
+
+const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) => {
+  const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
+
+  const { data: currentUser } = useCurrentUser();
+  const { mutate: mutatePosts } = usePosts();
+
   return (
     <>
       <div></div>
