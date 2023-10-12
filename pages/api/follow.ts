30706a0870ad27b9279d05078a9d2b119b270775
Adnan Mann <adnan_mann@outlook.com>
2023-10-12 15:43:09 +0100

    Created following hooks

--- pages/api/follow.ts
+++ pages/api/follow.ts
@@ -1,5 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
+import serverAuth from "@/libs/serverAuth";
+import prisma from "@/libs/prismadb";
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -11,7 +14,44 @@ export default async function handler(
   try {
     const { userId } = req.body;
 
-    const {};
+    const { currentUser } = await serverAuth(req, res);
+
+    if (!userId || typeof userId !== "string") {
+      throw new Error("Invalid ID");
+    }
+
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!user) {
+      throw new Error("Invalid ID");
+    }
+
+    let updatedFolloingIds = [...(user.folloingIds || [])];
+
+    if (req.method === "POST") {
+      updatedFolloingIds.push(userId);
+    }
+
+    if (req.method === "DELETE") {
+      updatedFolloingIds = updatedFolloingIds.filter(
+        (folloingId) => folloingId !== userId
+      );
+    }
+
+    const updatedUser = await prisma.user.update({
+      where: {
+        id: currentUser.id,
+      },
+      data: {
+        folloingIds: updatedFolloingIds,
+      },
+    });
+
+    return res.status(200).json(updatedUser);
   } catch (error) {
     console.log(error);
     return res.status(400).end();
