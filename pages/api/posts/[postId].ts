018a0ec8b57d26fe8db3445a837f47e01d113846
Adnan Mann <adnan_mann@outlook.com>
2023-10-16 17:30:36 +0100

    Returning response status

--- pages/api/posts/[postId].ts
+++ pages/api/posts/[postId].ts
@@ -33,6 +33,8 @@ export default async function handler(
         },
       },
     });
+
+    return res.status(200).json(post);
   } catch (error) {
     console.log(error);
     return res.status(400).end();
