f62a31654b564b03083e768d8a2ac204a2a5807f
Adnan Mann <adnan_mann@outlook.com>
2023-11-02 17:02:49 +0000

    fetched posts

--- hooks/useLike.ts
+++ hooks/useLike.ts
@@ -1,5 +1,7 @@
 import useCurrentUser from "./useCurrentUser";
+import usePost from "./usePost";
 
 const useLike = ({ postId, userId }: { postId: string; userId?: string }) => {
   const { data: currentUser } = useCurrentUser();
+  const { data: fetchedPost, mutate: mutuateFetchedPost } = usePost(postId);
 };
