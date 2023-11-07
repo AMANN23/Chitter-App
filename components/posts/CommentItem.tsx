38ffac45615642d3c7dfa60de4fcbe7562e21cd2
Adnan Mann <adnan_mann@outlook.com>
2023-11-07 12:36:51 +0000

    Showing when comment/reply was created

--- components/posts/CommentItem.tsx
+++ components/posts/CommentItem.tsx
@@ -1,4 +1,31 @@
-const CommentItem = () => {
+import { formatDistanceToNowStrict } from "date-fns";
+import { useRouter } from "next/router";
+import { useCallback, useMemo } from "react";
+
+interface CommentItemProps {
+  data: Record<string, any>;
+}
+
+const CommentItem: React.FC<CommentItemProps> = ({ data }) => {
+  const router = useRouter();
+
+  const goToUser = useCallback(
+    (event: any) => {
+      event.stopPropagation();
+
+      router.push(`/users/${data.user.id}`);
+    },
+    [router, data.user.id]
+  );
+
+  const createdAt = useMemo(() => {
+    if (!data?.createdAt) {
+      return null;
+    }
+
+    return formatDistanceToNowStrict(new Date(data.createdAt));
+  }, [data?.createdAt]);
+
   return (
     <>
       <div></div>
