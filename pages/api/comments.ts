67b3ef482d45c1438e89528bcb2eb8ffe062151e
Adnan Mann <adnan_mann@outlook.com>
2023-11-07 09:41:25 +0000

    Adding Comment

--- pages/api/comments.ts
+++ pages/api/comments.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 import serverAuth from "@/libs/serverAuth";
+import prisma from "@/libs/prismadb";
 
 export default async function handler(
   req: NextApiRequest,
@@ -18,6 +19,16 @@ export default async function handler(
     if (!postId || typeof postId !== "string") {
       throw new Error("Invalid ID");
     }
+
+    const comment = await prisma.comment.create({
+      data: {
+        body,
+        userId: currentUser.id,
+        postId,
+      },
+    });
+
+    return res.status(200).json(comment);
   } catch (error) {
     console.log(error);
     return res.status(400).end();
