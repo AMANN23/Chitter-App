c7566e3733984a5b3057f989160992ef6d3209d3
Adnan Mann <adnan_mann@outlook.com>
2023-11-07 10:22:34 +0000

    Minor adjustment

--- components/posts/commentFeed.tsx
+++ components/posts/commentFeed.tsx
@@ -2,10 +2,10 @@ interface CommentFeedProps {
   comments?: Record<string, any>[];
 }
 
-const CommentFeed: React.FC<CommentFeedProps> = ({ comments }) => {
+const CommentFeed: React.FC<CommentFeedProps> = ({ comments = [] }) => {
   return (
     <>
-      {comments?.map((comment) => (
+      {comments.map((comment) => (
         <div key={comment.id}></div>
       ))}
     </>
