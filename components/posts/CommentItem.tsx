db2f3758c8835327492a3f7a80405be6017fd627
Adnan Mann <adnan_mann@outlook.com>
2023-11-07 13:33:44 +0000

    Can see comment/reply on page

--- components/posts/CommentItem.tsx
+++ components/posts/CommentItem.tsx
@@ -67,6 +67,7 @@ const CommentItem: React.FC<CommentItemProps> = ({ data }) => {
               </span>
               <span className="text-neutral-500 text-sm">{createdAt}</span>
             </div>
+            <div className="text-white mt-1">{data.body}</div>
           </div>
         </div>
       </div>
