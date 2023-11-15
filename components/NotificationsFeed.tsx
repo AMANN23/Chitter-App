d84956598463e15a096224b0d59754e63f22f9a6
Adnan Mann <adnan_mann@outlook.com>
2023-11-15 18:21:22 +0000

    Liked chit will generate a notification but pages needs to be refreshed automatically

--- components/NotificationsFeed.tsx
+++ components/NotificationsFeed.tsx
@@ -1,6 +1,7 @@
 import useCurrentUser from "@/hooks/useCurrentUser";
 import useNotifications from "@/hooks/useNotifications";
 import { useEffect } from "react";
+import { BsTwitter } from "react-icons/bs";
 
 const NotificationsFeed = () => {
   const { data: currentUser, mutate: mutateCurrentUser } = useCurrentUser();
@@ -27,7 +28,25 @@ const NotificationsFeed = () => {
 
   return (
     <>
-      <div></div>
+      <div className="flex flex-col">
+        {fetchedNotifications.map((notification: Record<string, any>) => (
+          <div
+            key={notification.id}
+            className="
+            flex
+            flex-row
+            items-center
+            p-6
+            gap-4
+            border-b-[1px]
+            border-neutral-800
+            "
+          >
+            <BsTwitter color="white" size={32} />
+            <p className="text-white">{notification.body}</p>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
