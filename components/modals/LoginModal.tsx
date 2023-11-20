356246191353c6618cc2ebc0b73566f645f8d2c7
Adnan Mann <adnan_mann@outlook.com>
2023-11-20 15:16:27 +0000

    Making corrections to login screen

--- components/modals/LoginModal.tsx
+++ components/modals/LoginModal.tsx
@@ -62,7 +62,7 @@ const LoginModal = () => {
   const footerContent = (
     <div className="text-neutral-400 text-center mt-4">
       <p>
-        First time using Twitter?
+        First time using Chitter?
         <span
           onClick={onToggle}
           className="
